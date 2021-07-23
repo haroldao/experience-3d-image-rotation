b053377f203ab76387e1c7610c61a785ef26667a
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-07-23 22:57:32 +0200

    check if is-inview (IntersectionObserver)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import "./styles.scss";
 // import "./scroll";
+
+const sections = document.querySelectorAll("section")
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0.1) {
+            entry.target.classList.add("in-view");
+        } else {
+            entry.target.classList.remove("in-view");
+        }
+    })
+}, {
+    threshold: [0.1, 0.3]
+})
+
+sections.forEach(section => {
+  observer.observe(section)
+  console.log(section)
+})
