e13a3de8c57221890ee9ab64fe628910932b1198
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-07-23 22:19:44 +0200

    build

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -5905,7 +5905,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62426" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65328" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
