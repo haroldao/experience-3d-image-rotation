b2eeecd9d433e2a2144a2ec166dc767ef7e79d90
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-07-24 06:54:43 +0200

    mouseover images effect

--- src/index.js
+++ src/index.js
@@ -18,4 +18,20 @@ const observer = new IntersectionObserver(entries => {
 sections.forEach(section => {
   observer.observe(section)
   console.log(section)
+
+  const div = section.querySelector("div")
+  console.log(div)
+
+  const mq = window.matchMedia("(prefers-reduced-motion: no-preference)")
+
+  if(mq.matches) {
+      window.addEventListener("mousemove", (e) => {
+          const aimX = (e.clientX - (window.innerWidth / 2)) / 15
+          const aimY = (e.clientY - (window.innerHeight / 2)) / -10
+          div.style.transform = `rotateX(${aimY}deg) rotateY(${aimX}deg)`;
+      })
+  }
+  
+
 })
+
